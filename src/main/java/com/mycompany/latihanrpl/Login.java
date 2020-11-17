a2213600b07dde37390ce3cc0a83c76953698fc5
HezkielRivaldoSiregar <hezkiel.rivaldo@ti.ukdw.ac.id>
2020-11-17 23:05:40 +0700

    +penambahan interface login

--- src/main/java/com/mycompany/latihanrpl/Login.java
+++ src/main/java/com/mycompany/latihanrpl/Login.java
@@ -3,10 +3,14 @@ package com.mycompany.latihanrpl;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class PrimaryController {
+public class Login {
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+    
+    
+    
+    
 }
