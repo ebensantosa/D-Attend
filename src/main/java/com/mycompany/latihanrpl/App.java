bab0eeaa0962b54952a90f6577f85a07f2e922ff
Ebensantosa <ebentera.santosa@ti.ukdw.ac.id>
2020-11-21 23:18:18 +0700

    + Insert data daftar kelas oleh admin note : Belum melakukan select data

--- src/main/java/com/mycompany/latihanrpl/App.java
+++ src/main/java/com/mycompany/latihanrpl/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"),400,400);
+        scene = new Scene(loadFXML("login"),700,530);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/mycompany/latihanrpl/HomePage.java
+++ src/main/java/com/mycompany/latihanrpl/HomePage.java
@@ -1,21 +1,50 @@
 package com.mycompany.latihanrpl;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class HomePage {
+    
+    @FXML
+    private TextField kodeKelas;
+    @FXML
+    private TextField namaKelas;
+    @FXML
+    private TextField idDosen;
+    @FXML
+    private Label labelStatus;
+    
+    @FXML 
+    public void insert(ActionEvent event) throws SQLException{
+       if(kodeKelas.getText().isEmpty() == false && namaKelas.getText().isEmpty() == false && idDosen.getText().isEmpty() == false){
+           Connection conn = DBConnector.getInstance().getConnection();
+           String insertData = "INSERT INTO kelas values('" + kodeKelas.getText() + "','" + namaKelas.getText() + "','" + idDosen.getText() + "')";
+           Statement st = conn.createStatement();
+            try{
+                st.executeUpdate(insertData);
+                labelStatus.setText("Input Data Sukses");
+            }catch(Exception e){
+                e.printStackTrace();
+                labelStatus.setText("Input Data Gagal");
+            }
+       }else{
+           labelStatus.setText("Semua data harus terisi");
+       }
+    }
 
     @FXML
     private void Logout() throws IOException {
-//        Stage stage = new Stage();
-//        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-//        Scene scene = new Scene(root,400,400);
-//        stage.setScene(scene);
-//        stage.show();
           App.setRoot("login");
     }
-}
+    }
